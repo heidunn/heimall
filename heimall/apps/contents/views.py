0a76b9d60d31af0dabec9c330b5071616e961c09
heidunn <heidunn@163.com>
2021-05-29 01:04:53 +0800

    完善注册的功能

---
+++ heimall/apps/contents/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+    def post(self, request):
+        pass
+    pass
